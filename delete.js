1042de2917a1594325f5bc8959ce60a43f9da7c2
Christian Bundy <christianbundy@fraction.io>
2018-12-09 17:42:04 -0800

    Add example of deleting SSB feed by author

--- delete.js
+++ delete.js
@@ -8,36 +8,45 @@ const mv = require('mv')
 
 const paths = {
   db: path.join(__dirname, 'db'),
-  tmp: '/tmp/db'
+  tmp: '/tmp/db' + Math.random()
 }
 
 const createDb = (file) => flume(log(file, { codec: codec.json }))
 // a is the original log
 // b is our secondary temporary log
 var b = createDb(paths.tmp)
+
 // define function to exclude content (e.g. check `msg.value.author`)
 const shouldDelete = msg => msg.author === 'bob'
-
-// for each message, either ignore (delete) or add to new log
-const onEachMessage = (msg) => {
-  if (shouldDelete(msg)) {
-    console.log('deleted:', msg)
-  } else {
-    b.append(msg, function (err, seq) {
-      if (err) throw err
-      console.log('kept:   ', msg)
-    })
-  }
-}
 
 // once all of the non-deleted messages are added to the second db, we replace a with b
 
 module.exports = (obj, cb) => {
-  const db = obj.db
+  const { db, compare, file } = obj
+
+  let fn
+  if (typeof compare === 'function') {
+    fn = compare
+    console.log('is function')
+  } else {
+    fn = shouldDelete
+    console.log('not function')
+  }
+
+  // for each message, either ignore (delete) or add to new log
+  const onEachMessage = msg => {
+    if (fn(msg)) {
+      console.log('deleted:', msg)
+    } else {
+      b.append(msg, function (err, seq) {
+        if (err) throw err
+      })
+    }
+  }
 
   const onDone = () => {
     // overwrite the real db with the temporary db
-    mv(paths.tmp, paths.db, function (err) {
+    mv(paths.tmp, file || paths.db, function (err) {
       if (err) return cb(err)
       console.log('done with delete')
       cb(null)
---
+++ ssb.js
@@ -0,0 +1,21 @@
+const codec = require('flumecodec')
+const flume = require('flumedb')
+const log = require('flumelog-offset')
+const del = require('./delete')
+const os = require('os')
+const path = require('path')
+
+const createDb = (file) => flume(log(file, { codec: codec.json }))
+const homedir = os.homedir()
+const file = path.join(homedir, '.ssb/flume/log.offset')
+const db = createDb(file)
+
+const compare = msg => {
+  // ONLY RETURN TRUE WHEN MESSAGE SHOULD BE DELETED
+  return msg.value.author === '@+rMXLy1md42gvbBq+6l6rp95/drh6QyACO1ZZMMnWI0=.ed25519'
+}
+
+del({ db, compare, file }, (err) => {
+  if (err) throw err
+  console.log('done with operation')
+})
